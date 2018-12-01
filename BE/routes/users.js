f296da38f0c65c8c05424b976dda27ac0c1c552d
xpsilvester <wuxp5@mail2.sysu.edu.cn>
2018-12-01 22:04:30 +0800

    mod user

--- BE/routes/users.js
+++ BE/routes/users.js
@@ -1,41 +1,5 @@
 const express = require('express');
 const router = express.Router();
-//数据库
-const mongoose = require('../common/db');
-
-//用户数据集
-let user = new mongoose.Schema({
-  username: String,
-  password: String,
-  userMail: String,
-  userPhone: String,
-  userAdmin: Boolean,
-  userPower: Number,
-  userStop: Boolean,
-  updateTime: {type: Date, default: Date.now}
-})
-
-//用户的查找方法
-user.statics.findAll = (callBack) => {
-  this.find({},callBack)
-}
-
-//使用用户名查找的方式
-user.statics.findByUsername = (name,callBack) => {
-  this.find({username:name},callBack)
-}
-
-//登录匹配是不是拥有相同的用户名和密码并且没有处于封停状态
-user.statics.findUserLogin = (name,password,callBack) => {
-  this.find({username:name,password:password,userStop:false},callBack)
-}
-
-//验证邮箱、电话和用户名找到用户
-user.statics.findUserPassword = (name,mail,phone,callBack) => {
-  this.find({username:name,userMail:mail,userPhone:phone},callBack)
-}
-
-let userModel = mongoose.model('user',user);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -45,4 +9,3 @@ router.get('/', function(req, res, next) {
 
 
 module.exports = router;
-module.exports.userModel = userModel;
