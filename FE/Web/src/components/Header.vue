c23ba410417bc097c46fb32bcd3c55b6beac059c
xpsilvester <wuxp5@mail2.sysu.edu.cn>
2019-02-20 19:27:17 +0800

    mod construct

---
+++ FE/Web/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <div class="tag"></div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+header {
+  width: 100%;
+  height: 60px;
+  display: flex;
+  background: #55a52e;
+}
+</style>
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Vue+Express+Mongoose 全栈搭建电影网站
 
 ### FE
-前端代码
+前端代码: Web(前台页面)、CMS(后台管理页面)
 
 ### BE
 后端代码
